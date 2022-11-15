8592c45df953192ce5ef407c040a48dbb752851b
changhak0310 <dlckdgkr0320@naver.com>
2022-11-16 04:15:27 +0900

    Route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 //Pages
@@ -56,6 +56,7 @@ function App() {
           <Route element={<Todo/>} path="/todo"/>
           <Route element={<Login/>} path="/login"/>
           <Route element={<SignUp/>} path="/signup"/>
+          <Route element={<Navigate replace to="/user"/>} path="/" />
           <Route element={<PageNotFound/>} path="*" />
         </Routes> 
       </ThemeProvider>
